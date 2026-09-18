2b5984c556738dbc50b2b3f898317657adde078c
ThomasBecket <tegarsuryachristy.xrpl.mos@gmail.com>
2026-09-19 05:03:08 +0700

    Penyesuaian lagi dikit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="site-footer">
-      Copyright © — Tim Magang Unit Jaringan Komunikasi BBMKG Wilayah II 2026
+      <a href="/developer-info-page/developer-info-index.html">
+        Copyright © — Tim Magang Unit Jaringan Komunikasi BBMKG Wilayah II 2026
+      </a>
     </footer>
   );
 }
